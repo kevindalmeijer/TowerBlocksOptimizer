3a19591d3140d9e0270a2f41eb2322765149de1b
Kevin Dalmeijer <kevindalmeijer@gmail.com>
2024-09-06 00:02:24 -0400

    Cleaned up CP optimizer and updated main.

--- cp_optimizer.py
+++ cp_optimizer.py
@@ -2,172 +2,281 @@ import city
 import configuration
 from ortools.sat.python import cp_model
 import itertools as it
+import warnings
 
 
-class ConstraintProgrammingOptimizer:
+class CPOptimizer:
     """
     Optimizer based on constraint-programming solver CP-SAT.
     """
 
-    def __init__(self, city: city.City) -> None:
+    def __init__(self, city: city.City, settings: dict = {}) -> None:
+        """
+        Initialize the CPOptimizer object and check the settings provided by the user.
+
+        Args:
+            city (city.City): City for which the tower configuration is optimized.
+            settings (dict): Dictionary of user-provided parameters and values.
+                See __check_user_settings() for details.
+        """
         self.city = city
+        self.settings = settings
+        self.__check_user_settings()
+
+        n = self.city.n
+        m = self.city.m
+        self.nb_periods = min(n * m, settings["depth_limit"]) if "depth_limit" in self.settings else n * m
+
+        self.__build_model()
+
+    def __check_user_settings(self) -> None:
+        """
+        Check the settings provided by the user and give a warning for parameters that are not recognized.
+        """
+        parameters = [
+            "time_limit",   # time limit in seconds (default: no limit)
+            "depth_limit",  # maximum number of periods used by the model (default: city.n * city.m)
+            "print_log",    # print the CP-SAT log (default: False)
+        ]
+        for parameter in self.settings:
+            if parameter not in parameters:
+                warnings.warn(f"'{parameter}' is not a recognized parameter and will be ignored.", UserWarning)
 
     def run(self) -> tuple[configuration.Configuration, dict]:
-        
-        height = self.city.n
-        width = self.city.m
-        nb_colors = self.city.nb_colors
-        nb_periods = height * width
-        weights = self.city.scores        
-        
-        neighbors = tuple(
-            tuple(
-                tuple(
-                    {*{(k,j) for k in [i-1, i+1] if k >= 0 and k < height},
-                    *{(i,k) for k in [j-1, j+1] if k >= 0 and k < width}}
+        """
+        Run the optimization model.
+
+        Returns:
+            tuple(configuration.Configuration, dict): Best configuration that was found and
+            a dictionary with information about the solve.
+        """
+        status = self.solver.Solve(self.model)
+
+        solution = configuration.Configuration(self.city)
+        solution.towers = self.get_solution_towers()
+
+        info = dict()
+
+        # Solution is optimal if...
+        info["optimal"] = (status == 4)  # ...CP-SAT reports optimality, and...
+        required_depth = self.city.n * self.city.m  # ...the depth used is sufficient.
+        if self.settings.get("depth_limit", required_depth) < required_depth:
+            info["optimal"] = False
+
+        return solution, info
+
+    def get_solution_towers(self) -> list[list[int]]:
+        """
+        Get the tower placements found by the solver as a double array.
+
+        Returns:
+            list: Best found tower placements as a double array.
+        """
+        solution = [
+            [
+                sum(
+                    self.solver.Value(self.y[i, j, k]) * k
+                    for k in range(self.city.nb_colors)
                 )
-                for j in range(width)
-            )
-            for i in range(height)
-        )
-        
-        def get_solution(solver: cp_model.CpSolver):
-            solution = [
-                [
-                    sum(
-                        solver.Value(x[i, j, k, 0]) * k
-                        for k in range(nb_colors)
-                    )
-                    for j in range(width)
-                ]
-                for i in range(height)
+                for j in range(self.city.m)
             ]
-            return solution
+            for i in range(self.city.n)
+        ]
+        return solution
 
-        def get_solutions(solver: cp_model.CpSolver):
-            solution = [
-                [
-                    [
-                        sum(
-                            solver.Value(x[i, j, k, t]) * k
-                            for k in range(nb_colors)
-                        )
-                        for j in range(width)
-                    ]
-                    for i in range(height)
-                ]
-                for t in range(nb_periods)
-            ]
-            return solution
-        
-        ### CP MODEL ###
+    def __build_model(self) -> None:
+        """
+        Build the constraint programming model.
+        """
+        n = self.city.n
+        m = self.city.m
+        nb_colors = self.city.nb_colors
+        nb_periods = self.nb_periods
 
-        model = cp_model.CpModel()
-        solver = cp_model.CpSolver()
+        self.model = cp_model.CpModel()
+        self.solver = cp_model.CpSolver()
 
+        self.__set_solver_settings()
+        self.x, self.y = self.__define_variables(self.model, n, m, nb_colors, nb_periods)
+        self.__add_objective()
+        self.__add_constraints(self.model, self.x, n, m, nb_colors, nb_periods)
+
+    def __set_solver_settings(self) -> None:
+        """
+        Set solver settings based on the settings provided at initialization.
+        """
+        if "time_limit" in self.settings:
+            self.solver.parameters.max_time_in_seconds = self.settings['time_limit']
+        if "print_log" in self.settings:
+            self.solver.parameters.log_search_progress = self.settings["print_log"]
+
+    def __define_variables(
+        self,
+        model: cp_model.CpModel,
+        n: int,
+        m: int,
+        nb_colors: int,
+        nb_periods: int
+    ) -> tuple[dict, dict]:
+        """
+        Define variables and basic constraints for the optimization model.
+
+        Args:
+            model (cp_model.CpModel): CP-SAT model.
+            n (int): shorthand for self.city.n
+            m (int): shorthand for self.city.m
+            nb_colors (int): shorthand for self.city.nb_colors
+            nb_periods (int): shorthand for self.nb_periods
+
+        Returns:
+            tuple(dict, dict): variables x and variables y.
+        """
+
+        # Variables y represent the final configuration
+        y = {
+            (i, j, k):
+                model.NewBoolVar(f"y_{i}_{j}_{k}")
+            for i in range(n)
+            for j in range(m)
+            for k in range(nb_colors)
+        }
+
+        # Assign one color to each tower
+        for i in range(n):
+            for j in range(m):
+                model.AddExactlyOne(y[i, j, k] for k in range(nb_colors))
+
+        # Variables x represent the configuration in different periods
+        # from period 0 to nb_periods-1 (which matches y).
         x = {
-            (i, j, k, t): model.NewBoolVar("x_{}_{}_{}_{}".format(i, j, k, t))
-            for i in range(height)
-            for j in range(width)
+            (i, j, k, t):
+                model.NewBoolVar(f"x_{i}_{j}_{k}_{t}")
+            for i in range(n)
+            for j in range(m)
             for k in range(nb_colors)
             for t in range(nb_periods)
         }
 
-        solver.parameters.log_search_progress = True
-        # solver.parameters.max_time_in_seconds = 60
-
-        # Select one color per cell
-        for i in range(height):
-            for j in range(width):
+        # Assign one color to each tower in each period
+        for i in range(n):
+            for j in range(m):
                 for t in range(nb_periods):
-                    model.AddExactlyOne(
-                        x[i, j, k, t] for k in range(nb_colors)
-                    )
+                    model.AddExactlyOne(x[i, j, k, t] for k in range(nb_colors))
 
-        # End at all color zero
-        for i in range(height):
-            for j in range(width):
-                model.Add(x[i, j, 0, nb_periods-1] == True)
+        # x is zero in the first period...
+        for i in range(n):
+            for j in range(m):
+                model.Add(x[i, j, 0, 0] == 1)
 
-        # Objective
-        model.Maximize(
+        # ...and matches y in the final period
+        for i in range(n):
+            for j in range(m):
+                for k in range(nb_colors):
+                    model.Add(x[i, j, k, nb_periods - 1] == y[i, j, k])
+
+        return x, y
+
+    def __add_objective(self) -> None:
+        """
+        Add the objective to the optimization model.
+        """
+        self.model.Maximize(
             sum(
-                weights[k] * x[i, j, k, 0]
-                for i in range(height)
-                for j in range(width)
-                for k in range(nb_colors)
+                self.city.scores[k] * self.y[i, j, k]
+                for i in range(self.city.n)
+                for j in range(self.city.m)
+                for k in range(self.city.nb_colors)
             )
         )
 
-        for i in range(height):
-            for j in range(width):
-                for t1, t2 in zip(range(nb_periods), range(1, nb_periods)):
+    def __add_constraints(
+        self,
+        model: cp_model.CpModel,
+        x: dict,
+        n: int,
+        m: int,
+        nb_colors: int,
+        nb_periods: int
+    ) -> None:
+        """
+        Add necessary constraints to the optimization model.
 
-                    for k in range(nb_colors):
-                        
-                        # 1
-                        if k == 0:
-                            model.Add(x[i, j, 0, t2] >= x[i, j, 0, t1])
-                        else:
-                            model.Add(x[i, j, 0, t2] + x[i, j, k, t2] >= x[i, j, k, t1])
-                        
-                        # 2
-                        if k != 0:    
-                            model.Add(x[i, j, k, t2] >= x[i, j, k, t1] - sum(x[p, q, 0, t1] for p, q in neighbors[i][j]))
-                    
-                        # 3
-                        if k >= 2:
-                            nb_neighbors = len(neighbors[i][j])
-                            subset_size = nb_neighbors + 1 - k
-                            if subset_size < 0:
-                                continue  # weird edge case with 1 neighbor
-                            for subset in it.combinations(neighbors[i][j], subset_size):
-                                for l in range(k, nb_colors):
-                                    model.Add(
-                                        x[i, j, l, t2] >= sum(
-                                            x[p, q, m, t1]
-                                            for m in range(k, nb_colors)
-                                            for p, q in subset
-                                        ) - subset_size + x[i, j, l, t1]
-                                    )
+        Args:
+            model (cp_model.CpModel): CP-SAT model.
+            x (dict): shorthand for self.x
+            y (dict): shorthand for self.y
+            n (int): shorthand for self.city.n
+            m (int): shorthand for self.city.m
+            nb_colors (int): shorthand for self.city.nb_colors
+            nb_periods (int): shorthand for self.nb_periods
+        """
 
-                    # 4
-                    nb_neighbors = len(neighbors[i][j])
-                    if nb_neighbors >= 3:
-                        subset_size = nb_neighbors - 1
-                        for subset in it.combinations(neighbors[i][j], subset_size):
-                            model.Add(
-                                x[i, j, 3, t2] >= sum(
-                                    x[p, q, k, t1]
-                                    for k in [1, 3]
-                                    for p, q in subset
-                                ) - subset_size + x[i, j, 3, t1]
+        # Going back in time, maintain 0-towers
+        for i in range(n):
+            for j in range(m):
+                for s, t in zip(range(nb_periods), range(1, nb_periods)):
+                    model.Add(
+                        x[i, j, 0, s] >= x[i, j, 0, t]
+                    )
+
+        # Going back in time, maintain towers or reduce to 0
+        for i in range(n):
+            for j in range(m):
+                for s, t in zip(range(nb_periods), range(1, nb_periods)):
+                    for k in range(1, nb_colors):
+                        model.Add(
+                            x[i, j, 0, s] + x[i, j, k, s] >= x[i, j, k, t]
+                        )
+
+        # Going back in time, maintain towers that don't have 0-neighbors
+        for i in range(n):
+            for j in range(m):
+                for s, t in zip(range(nb_periods), range(1, nb_periods)):
+                    for k in range(1, nb_colors):
+                        model.Add(
+                            x[i, j, k, s] >= x[i, j, k, t] - sum(
+                                x[p, q, 0, t] for p, q in self.city.neighbors(i, j)
                             )
+                        )
 
-        # for i in range(height):
-        #     for j in range(width):
-        #         for t1, t2 in zip(range(nb_periods), range(1, nb_periods)):
-                    
-        #             for k in range(1, nb_colors):
-                        
-        #                 permutation_size = k
-        #                 for permutation in it.permutations(neighbors[i][j], permutation_size):
-                            
-        #                     model.Add(
-        #                         x[i, j, 0, t2] >= sum(
-        #                             x[permutation[r][0], permutation[r][1], l, t1]
-        #                             for r in range(permutation_size)
-        #                             for l in set([0, r])
-        #                         ) - permutation_size + x[i, j, k, t1]
-        #                     )       
+        # Going back in time, maintain a-towers if for any threshold (k-1) < a
+        # there are too many towers b >= k that do not contribute to its reduction.
+        # Example for a tower of color 3 (a=3) with four neighbors using threshold 1 (k=2):
+        #   Maintain this 3-tower if three or more of its neighbors have color >= 2.
+        #   To reduce the 3-tower, it needs at least one neighbor of color 0,
+        #   so if three neighbors have color >= 2, then no 1-tower is available for the reduction.
+        for i in range(n):
+            for j in range(m):
+                neighbors = self.city.neighbors(i, j)
+                for s, t in zip(range(nb_periods), range(1, nb_periods)):
+                    for k in range(2, nb_colors):
+                        subset_size = len(neighbors) + 1 - k
+                        if subset_size < 0:
+                            continue  # handle edge case when len(neighbors) = 1
+                        for subset in it.combinations(neighbors, subset_size):
+                            for a in range(k, nb_colors):
+                                model.Add(
+                                    x[i, j, a, s] >= x[i, j, a, t] + sum(
+                                        x[p, q, b, t]
+                                        for b in range(k, nb_colors)
+                                        for p, q in subset
+                                    ) - subset_size
+                                )
 
-        status = solver.Solve(model)
-        
-        info = dict()
-        info["optimal"] = True if status == 4 else False
-            
-        solutions = get_solutions(solver)
-        solution = configuration.Configuration(self.city)
-        solution.towers = solutions[0]
-        
-        return solution, info
+        # Correction to forbid neighbors 0/1/1 to reduce 3-towers,
+        # which is not captured by the threshold rule above.
+        for i in range(n):
+            for j in range(m):
+                neighbors = self.city.neighbors(i, j)
+                if len(neighbors) < 3:
+                    continue
+                subset_size = len(neighbors) - 1
+                for s, t in zip(range(nb_periods), range(1, nb_periods)):
+                    for subset in it.combinations(neighbors, subset_size):
+                        model.Add(
+                            x[i, j, 3, s] >= x[i, j, 3, t] + sum(
+                                x[p, q, k, t]
+                                for k in [1, 3]
+                                for p, q in subset
+                            ) - subset_size
+                        )
--- main.py
+++ main.py
@@ -5,12 +5,16 @@ import pprint
 
 my_city = city.City(5, 5, scores=[205, 966, 2677, 5738])
 my_solver = solver.Solver(my_city)
-my_optimizer = cp_optimizer.ConstraintProgrammingOptimizer(my_city)
 
-solution, info = my_solver.solve(my_optimizer)
+settings = {
+    'print_log': True,
+}
+optimizer = cp_optimizer.CPOptimizer(my_city, settings)
+
+solution, info = my_solver.solve(optimizer)
 pprint.pprint(info)
 print(
-    f"Solution with total score {info['total_score']} (optimal={info['optimal']})" +
-    f"obtained in {len(info["moves"])} moves:"
+    f"Solution with total score {info['total_score']} (optimal={info['optimal']}) " +
+    f"obtained in {len(info['moves'])} moves:"
 )
 print(solution)
